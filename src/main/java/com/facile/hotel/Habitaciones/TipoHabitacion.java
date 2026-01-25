f414fb766894692d1fe49b4a0589686a744d968b
Sebastian-122 <sebastianparada122q@gmail.com>
2026-01-25 10:26:38 -0500

    Delete src directory

--- src/main/java/com/facile/hotel/Habitaciones/TipoHabitacion.java
+++
@@ -1,7 +0,0 @@
-package com.facile.hotel.habitaciones;
-
-public enum TipoHabitacion {
-    HABITACION_SENCILLA,
-    APARTAMENTO_SIMPLE,
-    APARTAMENTO_DUPLEX
-}
--- src/main/java/com/facile/hotel/roles/RolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.facile.hotel.roles;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol, Long> {
-    Optional<Rol> findByNombre(String nombre);
-}
